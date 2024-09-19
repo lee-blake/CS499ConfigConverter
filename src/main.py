5677d326d5ba7fa4dbbb7287929b997509c95b9e
Lee Blake <122327096+lee-blake@users.noreply.github.com>
2024-09-18 20:00:18 -0400

    Implement file output with argparse

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+#!/bin/python
+import argparse
 import codecs
 import sys
 
@@ -5,9 +7,35 @@ from config_file_converter import ConfigFileConverter
 from ipv4_prefix import IPv4Prefix
 from topology import TopologyBuilder
 
+
+
+class PrintWriter:
+        def write(self, stuff):
+            print(stuff, end="")
+
+        def close(self):
+            pass
+
+def setup_parser():
+    parser = argparse.ArgumentParser(prog="python src/main.py")
+    parser.add_argument("input_files", nargs="+", help="Path to the files to parse to build the topology.")
+    parser.add_argument("--out-file", help="Path to the file to output the converted config.")
+    return parser
+
+def get_output_handle(output_filename):
+    if not output_filename:
+        return PrintWriter()
+    return open(output_filename, "wt")
+
 def main(argv):
+    # Throw away this file if "python *main.py" is invoked, it should
+    # not be an argument at all.
+    if "main.py" in argv[0]:
+        argv = argv[1:]
+    arg_parser = setup_parser()
+    args = arg_parser.parse_args()
     topology_builder = TopologyBuilder()
-    filenames = argv[1:]
+    filenames = args.input_files
     for filename in filenames:
         # Original configs are NOT UTF-8. Latin1 seems to decode correctly,
         # so we shall use that instead.
@@ -19,10 +47,9 @@ def main(argv):
             print(f"Error in {filename}")
             print(e)
         fh.close()
-    class PrintWriter:
-        def write(self, stuff):
-            print(stuff, end="")
-    topology_builder.write_topology(PrintWriter())
+    output_handle = get_output_handle(args.out_file)
+    topology_builder.write_topology(output_handle)
+    output_handle.close()
 
 
 if __name__ == "__main__":
